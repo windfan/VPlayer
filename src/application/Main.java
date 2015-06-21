1a45b1db996d73e9dcff2260ff8b771fe539487b
Victor <ryu1031@gmail.com>
2015-06-21 17:53:46 -0400

    Get rid of nonuse import

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 
 
